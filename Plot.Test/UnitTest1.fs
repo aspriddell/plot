d4bebc160eeb21b047f34ec6de2515b08cfee581
Leon D'Exelle <37039203+leondexelle@users.noreply.github.com>
2024-10-12 16:54:21 +0100

    Reformat

--- Plot.Test/UnitTest1.fs
+++ Plot.Test/UnitTest1.fs
@@ -4,11 +4,10 @@ open NUnit.Framework
 open Plot.Core
 
 [<SetUp>]
-let Setup () =
-    ()
+let Setup () = ()
 
 [<Test>]
 let Test1 () =
-    let input = "8.5 + 1\n10 * 2\n"
-    let tokens = Lexer.Parse (input.ToCharArray())
+    let input = "x = 8.5 + 1\n10 * x\n"
+    let tokens = Lexer.Parse(input.ToCharArray())
     Parser.ParseAndEval tokens
