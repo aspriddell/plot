59ea87ffcc63ab43404243bdde408a215ff29293
Albie Spriddell <albion@dragonfruit.network>
2024-10-13 20:34:33 +0100

    initial ui pass (some buttons non-functional)

--- Plot/ViewModels/MainWindowViewModel.cs
+++ Plot/ViewModels/MainWindowViewModel.cs
@@ -1,9 +1,52 @@
-﻿using ReactiveUI;
+﻿using System;
 using System.Windows.Input;
+using AvaloniaEdit.Document;
+using Microsoft.FSharp.Collections;
+using Plot.Core;
+using ReactiveUI;
 
 namespace Plot.ViewModels
 {
     public class MainWindowViewModel : ReactiveObject
     {
+        public MainWindowViewModel()
+        {
+            ClearOutput = ReactiveCommand.Create(() => OutputDocument.Text = "");
+            ExecuteSource = ReactiveCommand.Create(ParseAndExecute);
+        }
+
+        public TextDocument SourceDocument { get; } = new();
+        public TextDocument OutputDocument { get; } = new();
+        
+        public ICommand ClearOutput { get; }
+        public ICommand ExecuteSource { get; }
+
+        private void ParseAndExecute()
+        {
+            if (SourceDocument.TextLength == 0 || string.IsNullOrWhiteSpace(SourceDocument.Text))
+            {
+                return;
+            }
+
+            try
+            {
+                var tokenChain = Lexer.Parse(SourceDocument.Text);
+
+                OutputDocument.Insert(OutputDocument.TextLength, $"\n----- RUN {DateTime.Now:G} -----\n\n");
+
+                foreach (var outputToken in Parser.ParseAndEval(tokenChain))
+                {
+                    OutputDocument.Insert(OutputDocument.TextLength, $"> {outputToken}\n");
+                }
+            }
+            catch (Lexer.LexerException e)
+            {
+                OutputDocument.Insert(OutputDocument.TextLength, $"\n----- {e.Message} -----\n");
+            }
+            catch (Exception e)
+            {
+                OutputDocument.Insert(OutputDocument.TextLength, $"\n----- Unexpected error: {e.Message} -----\n");
+            }
+        }
     }
 }
---
+++ Plot/Views/MainWindow.axaml.cs
@@ -0,0 +1,16 @@
+using FluentAvalonia.UI.Windowing;
+
+namespace Plot.Views;
+
+public partial class MainWindow : AppWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        TitleBar.ExtendsContentIntoTitleBar = true;
+        TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
+
+        TransparencyLevelHint = App.TransparencyLevels;
+    }
+}
