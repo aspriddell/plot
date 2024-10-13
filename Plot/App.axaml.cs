ca63086cca17266313413313948f8060f26f72a1
Albie Spriddell <albion@dragonfruit.network>
2024-10-13 20:34:14 +0100

    add fonts/style libs

--- Plot/App.axaml.cs
+++ Plot/App.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+using System.Collections.Generic;
 using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using Plot.ViewModels;
@@ -7,6 +10,28 @@ namespace Plot;
 
 public partial class App : Application
 {
+    /// <summary>
+    /// Transparency level hints passed to windows to enable transparency effects (if supported).
+    /// </summary>
+    public static readonly IReadOnlyList<WindowTransparencyLevel> TransparencyLevels;
+
+    public static string Name => "Plot";
+    public static string Version { get; }
+    
+    static App()
+    {
+        // enable mica effect on Windows 11 and above
+        TransparencyLevels = OperatingSystem.IsWindowsVersionAtLeast(10, 22000)
+            ? [WindowTransparencyLevel.Mica, WindowTransparencyLevel.AcrylicBlur]
+            : [WindowTransparencyLevel.AcrylicBlur];
+        
+#if DEBUG
+        Version = "Dev Edition";
+#else
+        Version = $"v{typeof(App).Assembly.GetName().Version?.ToString(2)}" ?? "Unknown";
+#endif
+    }
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -16,7 +41,7 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new MainWindow
+            desktop.MainWindow = new Views.MainWindow
             {
                 DataContext = new MainWindowViewModel()
             };
