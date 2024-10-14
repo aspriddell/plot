2c3044915a385b1e9f5ee80b05d33a07298e9b7c
Albie Spriddell <albion@dragonfruit.network>
2024-10-14 16:13:06 +0100

    fix hotkeys on windows

--- Plot/Views/MainWindow.axaml.cs
+++ Plot/Views/MainWindow.axaml.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Linq;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
 using FluentAvalonia.UI.Windowing;
+using Plot.ViewModels;
 
 namespace Plot.Views;
 
@@ -12,5 +18,35 @@ public partial class MainWindow : AppWindow
         TitleBar.TitleBarHitTestType = TitleBarHitTestType.Complex;
 
         TransparencyLevelHint = App.TransparencyLevels;
+    }
+
+    protected override void OnLoaded(RoutedEventArgs _)
+    {
+        if (OperatingSystem.IsWindows() && DataContext is MainWindowViewModel _)
+        {
+            RegisterHotKeys(NativeMenu.GetMenu(this));
+        }
+    }
+
+    // reigster hotkeys on platforms that don't "just work" out the box
+    // loosely based on https://github.com/AvaloniaUI/Avalonia/issues/2441#issuecomment-2151522663
+    private void RegisterHotKeys(NativeMenu control)
+    {
+        foreach (var item in control.Items.OfType<NativeMenuItem>())
+        {
+            if (item.Command != null && item.Gesture != null)
+            {
+                KeyBindings.Add(new KeyBinding
+                {
+                    Gesture = item.Gesture,
+                    Command = item.Command
+                });
+            }
+
+            foreach (var childItem in item.Menu?.OfType<NativeMenuItem>() ?? [])
+            {
+                RegisterHotKeys(childItem.Parent);
+            }
+        }
     }
 }
