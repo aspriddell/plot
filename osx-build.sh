e071b126c091796033e9be3db2c7d2939a2ad89c
Albie Spriddell <albion@dragonfruit.network>
2024-10-15 14:02:25 +0100

    add macos bundle support

---
+++ osx-build.sh
@@ -0,0 +1,20 @@
+rm -rf Plot.app
+rm -f Plot.app.zip
+
+# prepare the app bundle
+mkdir -p Plot.app/Contents/MacOS
+cp Info.plist Plot.app/Contents
+
+# build directly into the app bundle
+dotnet publish -c Release -p:Version=$1 --self-contained -r osx-arm64 -o ./Plot.app/Contents/MacOS Plot/Plot.csproj
+
+# perform signing with timestamp
+codesign --force \
+  --deep \
+  --timestamp \
+  --sign "Developer ID Application: Albie Spriddell (Q824VHAT9S)" \
+  --entitlements app.entitlements \
+  Plot.app
+
+# create a zip file
+ditto -ck --sequesterRsrc --keepParent Plot.app Plot.app.zip
