0821b456a1cbaefdcc59887c681b5c850e873a9d
Albie Spriddell <albion@dragonfruit.network>
2024-10-15 14:28:26 +0100

    fix notarization issues

--- osx-build.sh
+++ osx-build.sh
@@ -9,12 +9,24 @@ cp Info.plist Plot.app/Contents
 dotnet publish -c Release -p:Version=$1 --self-contained -r osx-arm64 -o ./Plot.app/Contents/MacOS Plot/Plot.csproj
 
 # perform signing with timestamp
-codesign --force \
-  --deep \
-  --timestamp \
-  --sign "Developer ID Application: Albie Spriddell (Q824VHAT9S)" \
+codesign --sign "Developer ID Application: Albie Spriddell (Q824VHAT9S)" \
   --entitlements app.entitlements \
+  --options=runtime \
+  --timestamp \
+  --no-strict \
+  --force \
+  --deep \
   Plot.app
 
 # create a zip file
+ditto -ck --sequesterRsrc --keepParent Plot.app Plot.app.zip
+
+# submit for notarization
+xcrun notarytool submit Plot.app.zip \
+  --keychain-profile "notarytool" \
+  --progress \
+  --wait
+  
+# staple the notarization ticket and rearchive
+xcrun stapler staple Plot.app
 ditto -ck --sequesterRsrc --keepParent Plot.app Plot.app.zip
