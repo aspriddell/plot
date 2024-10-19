6e858e6f08ff3f6d030451b0b1c76b7834353510
Albie Spriddell <albion@dragonfruit.network>
2024-10-19 14:33:09 +0100

    add token splitter test

---
+++ Plot.Test/TokenSplittingTests.fs
@@ -0,0 +1,13 @@
+﻿module Plot.Test.TokenSplittingTests
+
+open NUnit.Framework
+open Plot.Core
+
+let TokenSplitTestCases: TestCaseData list = [
+    TestCaseData([NumI(100); Add; Identifier("x"); Comma; NumI(200); Comma; Identifier("a")], [[NumI(100); Add; Identifier("x")]; [NumI(200)]; [Identifier("a")]])
+]
+
+[<TestCaseSource(nameof TokenSplitTestCases)>]
+let TestTokenSplitting (input: TokenType list, output: TokenType list list) =
+    let outputSeq = TokenUtils.splitTokenArguments input
+    Assert.That(outputSeq, Is.EqualTo(output))
