210fbd15ee3316e670c799d24a4abcd693d5a627
Albie Spriddell <albion@dragonfruit.network>
2024-10-19 14:34:23 +0100

    add basic trigonometry functions

---
+++ Plot.Core/Functions/Trig.fs
@@ -0,0 +1,24 @@
+﻿module Plot.Core.Functions.Trig
+
+open Plot.Core.Symbols
+
+let public sin (x: SymbolType list) : SymbolType =
+    match x |> List.head with
+    | Int i -> Float(float i |> sin)
+    | Float f -> Float(f |> sin)
+
+    | _ -> invalidOp "Sin not defined for the given type"
+    
+let public cos (x: SymbolType list) : SymbolType =
+    match x |> List.head with
+    | Int i -> Float(float i |> cos)
+    | Float f -> Float(f |> cos)
+
+    | _ -> invalidOp "Cos not defined for the given type"
+    
+let public tan (x: SymbolType list) : SymbolType =
+    match x |> List.head with
+    | Int i -> Float(float i |> tan)
+    | Float f -> Float(f |> tan)
+
+    | _ -> invalidOp "Tan not defined for the given type"
