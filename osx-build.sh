dcd3c79f43e2c3435eeef74e746547017649dbb0
Albie Spriddell <albion@dragonfruit.network>
2024-10-21 20:38:27 +0100

    add dual build/sign config

--- osx-build.sh
+++ osx-build.sh
@@ -1,32 +1,41 @@
-rm -rf Plot.app
-rm -f Plot.app.zip
+build_and_notarize() {
+  local runtime=$1
+  local suffix=$2
+  local version=$3
 
-# prepare the app bundle
-mkdir -p Plot.app/Contents/MacOS
-cp Info.plist Plot.app/Contents
+  rm -rf Plot.app
+  rm -f Plot.app.zip
 
-# build directly into the app bundle
-dotnet publish -c Release -p:Version=$1 --self-contained -r osx-arm64 -o ./Plot.app/Contents/MacOS Plot/Plot.csproj
+  # prepare the app bundle
+  mkdir -p Plot.app/Contents/MacOS
+  cp Info.plist Plot.app/Contents
 
-# perform signing with timestamp
-codesign --sign "Developer ID Application: Albie Spriddell (Q824VHAT9S)" \
-  --entitlements app.entitlements \
-  --options=runtime \
-  --timestamp \
-  --no-strict \
-  --force \
-  --deep \
-  Plot.app
+  # build directly into the app bundle
+  dotnet publish -c Release -p:Version=$version --self-contained -r $runtime -o ./Plot.app/Contents/MacOS Plot/Plot.csproj
 
-# create a zip file
-ditto -ck --sequesterRsrc --keepParent Plot.app Plot.app.zip
+  # perform signing with timestamp
+  codesign --sign "Developer ID Application: Albie Spriddell (Q824VHAT9S)" \
+    --entitlements app.entitlements \
+    --options=runtime \
+    --timestamp \
+    --no-strict \
+    --force \
+    --deep \
+    Plot.app
 
-# submit for notarization
-xcrun notarytool submit Plot.app.zip \
-  --keychain-profile "notarytool" \
-  --progress \
-  --wait
-  
-# staple the notarization ticket and rearchive
-xcrun stapler staple Plot.app
-ditto -ck --sequesterRsrc --keepParent Plot.app Plot.app.zip
+  # create a zip file
+  ditto -ck --sequesterRsrc --keepParent Plot.app Plot.app.$suffix.zip
+
+  # submit for notarization
+  xcrun notarytool submit Plot.app.$suffix.zip \
+    --keychain-profile "notarytool" \
+    --progress \
+    --wait
+
+  # staple the notarization ticket and rearchive
+  xcrun stapler staple Plot.app
+  ditto -ck --sequesterRsrc --keepParent Plot.app Plot.app.$suffix.zip
+}
+
+build_and_notarize "osx-x64" "Intel" $1
+build_and_notarize "osx-arm64" "Apple.Silicon" $1
