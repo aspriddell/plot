ccf1c150ac03645082d196b59a3912610178b654
Leon D'Exelle <37039203+leondexelle@users.noreply.github.com>
2024-10-21 16:53:37 +0100

    Create Base PlotScript functions file and add pow

---
+++ Plot.Core/Functions/Base.fs
@@ -0,0 +1,15 @@
+module Plot.Core.Functions.Base
+
+open Plot.Core
+open Plot.Core.Symbols
+
+[<PlotScriptFunction("pow")>]
+let public pow (x: SymbolType list) : SymbolType =
+    match x with
+    | [ Int i1; Int i2 ] -> Int(int (float i1 ** i2))
+    | [ Int i; Float f ] -> Float(float i ** f)
+    | [ Float f; Int i ] -> Float(f ** float i)
+    | [ Float f1; Float f2 ] -> Float(f1 ** f2)
+
+    | [ _; _ ] -> invalidOp "pown not defined for the given types"
+    | _ -> invalidArg "*" "pown expects two arguments"
