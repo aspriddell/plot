76f050143a8aacee690cc64aa14d3a6026a60311
Albie Spriddell <albion@dragonfruit.network>
2024-10-28 16:25:32 +0000

    type fix

--- Plot.Core/Functions/Consts.fs
+++ Plot.Core/Functions/Consts.fs
@@ -5,21 +5,21 @@ open Plot.Core
 open Plot.Core.Symbols
 
 [<PlotScriptFunction("pi")>]
-let pi (tokens: TokenType list): SymbolType =
+let pi (tokens: SymbolType list) : SymbolType =
     if tokens.Length <> 0 then
         raise (invalidArg "pi" "pi does not take any arguments")
 
     Float(Math.PI)
 
 [<PlotScriptFunction("euler")>]
-let euler (tokens: TokenType list): SymbolType =
+let euler (tokens: SymbolType list) : SymbolType =
     if tokens.Length <> 0 then
         raise (invalidArg "euler" "euler does not take any arguments")
 
     Float(Math.E)
-    
+
 [<PlotScriptFunction("tau")>]
-let tau (tokens: TokenType list): SymbolType =
+let tau (tokens: SymbolType list) : SymbolType =
     if tokens.Length <> 0 then
         raise (invalidArg "tau" "tau does not take any arguments")
 
