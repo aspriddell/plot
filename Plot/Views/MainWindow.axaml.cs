306273aa870fca8b5b6905645976b39e9181da67
Albie Spriddell <albion@dragonfruit.network>
2024-11-06 13:54:16 +0000

    move keybinding logic for windows to reactiveappwindow

--- Plot/Views/MainWindow.axaml.cs
+++ Plot/Views/MainWindow.axaml.cs
@@ -1,16 +1,10 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Reactive;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Threading.Tasks;
-using Avalonia.Controls;
-using Avalonia.Input;
-using Avalonia.Interactivity;
 using Avalonia.Platform.Storage;
-using Avalonia.Threading;
 using FluentAvalonia.UI.Windowing;
 using Plot.ViewModels;
 using ReactiveUI;
@@ -43,39 +37,6 @@ public partial class MainWindow : ReactiveAppWindow<MainWindowViewModel>
                 .Subscribe(e => EnsureGraphWindow(!e.TabChanged))
                 .DisposeWith(disposables);
         });
-    }
-
-    protected override void OnLoaded(RoutedEventArgs e)
-    {
-        if (OperatingSystem.IsWindows() && DataContext is MainWindowViewModel _)
-        {
-            RegisterHotKeys(NativeMenu.GetMenu(this));
-        }
-
-        // needed to allow WhenActivated calls to fire
-        base.OnLoaded(e);
-    }
-
-    // register hotkeys on platforms that don't "just work" out the box
-    // loosely based on https://github.com/AvaloniaUI/Avalonia/issues/2441#issuecomment-2151522663
-    private void RegisterHotKeys(NativeMenu control)
-    {
-        foreach (var item in control.Items.OfType<NativeMenuItem>())
-        {
-            if (item.Command != null && item.Gesture != null)
-            {
-                KeyBindings.Add(new KeyBinding
-                {
-                    Gesture = item.Gesture,
-                    Command = item.Command
-                });
-            }
-
-            foreach (var childItem in item.Menu?.OfType<NativeMenuItem>() ?? [])
-            {
-                RegisterHotKeys(childItem.Parent);
-            }
-        }
     }
 
     private async Task CopyToClipboard(InteractionContext<string, Unit> ctx)
@@ -116,7 +77,7 @@ public partial class MainWindow : ReactiveAppWindow<MainWindowViewModel>
 
         if (!_graphWindow.IsVisible)
         {
-            _graphWindow.Show();
+            _graphWindow.Show(this);
         }
 
         if (activateWindow)
